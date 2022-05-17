8255cdaae85a38e4548e97bc9a55d7e1f52909cb
lalitnaj <86791474+lalitnaj@users.noreply.github.com>
2022-05-17 19:23:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
 import firebase from "firebase";
 require("@firebase/firestore");
 
-var firebaseConfig = {
-  apiKey: "AIzaSyCZrG0BF6LrnRpWZjsPQxNGyMCLByo8HrM",
-  authDomain: "e-ride-549e4.firebaseapp.com",
-  projectId: "e-ride-549e4",
-  storageBucket: "e-ride-549e4.appspot.com",
-  messagingSenderId: "588298110700",
-  appId: "1:588298110700:web:34e3df331c6b1fc95047c9",
-  measurementId: "G-G3K7BWL5LH"
+const firebaseConfig = {
+  apiKey: "AIzaSyAzriu6LI_KhBR4IZOMvDF6zsAKMvqaKic",
+  authDomain: "e-library-3c716.firebaseapp.com",
+  projectId: "e-library-3c716",
+  storageBucket: "e-library-3c716.appspot.com",
+  messagingSenderId: "380670574962",
+  appId: "1:380670574962:web:bef37400f4b49fbce0ddeb"
 };
 
 firebase.initializeApp(firebaseConfig);
